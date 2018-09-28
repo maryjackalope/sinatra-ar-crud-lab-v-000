c8512c00d4d7f99674b0de14b9e9c5b61abcf516
Mary Jack <maryjackinthebox@gmail.com>
2018-09-28 15:51:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,42 @@
-
 require_relative '../../config/environment'
-
-class ApplicationController < Sinatra::Base
-
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
+ 	class ApplicationController < Sinatra::Base
+   configure do	  
+    set :public_folder, 'public'	    
+    set :views, 'app/views'	  
+  end	  
+   get '/' do	  get '/posts/new' do #loads new form
+     erb :new
+  end	  end
+end	
+  post '/posts' do  #creates a post
+    @post = Post.create(params)
+    redirect to '/posts'
   end
-
-  get '/' do
-
+   get '/posts' do #loads index page
+    @posts = Post.all
+    erb :index
   end
-end
+   get '/posts/:id' do  #loads show page
+    @post = Post.find_by_id(params[:id])
+    erb :show
+  end
+  get '/posts/:id/edit' do 
+    @post = Post.find_by_id(params[:id])
+    erb :edit
+  end 
+  
+  patch '/posts/:id' do  #updates a post
+    @post = Post.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+  delete '/posts/:id/delete' do 
+    @post = Post.find_by_id(params[:id])
+    @post.delete
+    
+    erb :deleted
+    
+  end 
+end  
